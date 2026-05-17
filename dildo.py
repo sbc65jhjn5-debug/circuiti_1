0af078fdce94d3ba82f333618e4f1418856b753f
Valeria Scarabelli <v.scarabelli@icloud.com>
2026-05-17 20:01:40 +0200

    HELLO

--- dildo.py
+++ dildo.py
@@ -2,12 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from iminuit import Minuit
 from iminuit.cost import LeastSquares
+from scipy.stats import chi2
 
 # Legge di Shockley:
 q_KT = 38.6 # V^-1
 
-def I_shockley (V, I_0, g):
-    return I_0 * (np.exp (q_KT * V) / g - 1)
+def I_shockley (V, I_0, g, c):
+    return I_0 * (np.exp (q_KT * V / g) - 1) + c
 
 # Prendiamo (come resistore) nelle due configurazioni V e I
 
@@ -27,9 +28,21 @@ def I_shockley (V, I_0, g):
 V_1 = np.array ([0.102, 0.152, 0.173, 0.203, 0.222, 0.252, 0.272, 0.302, 0.322, 0.352, 0.373, 0.403]) #V    (Da [0.352, 5.42] errore più alto su I vedi video 4)
 I_1 = np.array ([0.008, 0.035, 0.058, 0.123, 0.203, 0.431, 0.705, 1.521, 2.550, 5.42, 9.16, 18.60]) #e-6 A
 
-sigma_I_1 = np.ones (9) * 0.00205 #microA
-sigma_I_2 = np.ones (3) * 0.00202 #microA
-sigma_I_conf1 = sigma_I_1 + sigma_I_2
+sigma_I_11 = np.ones (9) * 0.00205 #microA
+sigma_I_12 = np.ones (3) * 0.00202 #microA
+sigma_I_conf1 = np.array ([0.00205,
+                           0.00205,
+                           0.00205,
+                           0.00205,
+                           0.00205,
+                           0.00205,
+                           0.00205,
+                           0.00205,
+                           0.00205,
+                           0.00202,
+                           0.00202,
+                           0.00202
+                           ])
 
 #Configurazione 2: (err_I --> video 5)
 V_2 = np.array ([0.052, 0.072, 0.102, 0.122, 0.152, 0.173, 0.202, 0.222, 0.252, 0.272, 0.301, 0.321, 0.351, 0.372, 0.399]) # V    (Da [0.222, 0.221] errore su I video 6)
@@ -45,7 +58,8 @@ ls = LeastSquares (V_1,
 
 m = Minuit (ls, 
             I_0 = 0.0001,
-            g = 1.5
+            g = 1.5,
+            c = 0
             )
 
 m.migrad ()
@@ -53,12 +67,33 @@ m.migrad ()
 for par, val, err in zip (m.parameters, m.values, m.errors):
     print (f"{par} = {val:.4f} \pm {err:.4f}")
     
-I_0_fit = m.values["I_0"]
-g_fit = m.values["g"]
+I_0_fit_1 = m.values["I_0"]
+g_fit_1 = m.values["g"]
+c_fit_1 = m.values["c"]
 
 fig, ax = plt.subplots ()
+
+x_axis_1 = np.linspace (0.07, max(V_2) + 0.01, 5000)
 
 ax.set_title ("Caratterizzazione del diodo")
 ax.set_xlabel ("Tensione (V)")
 ax.set_ylabel ("Intensità di corrente ($\\mu$A)")
 
+ax.plot (x_axis_1,
+         [I_shockley(x, I_0_fit_1, g_fit_1, c_fit_1) for x in x_axis_1],
+         label = "Fit con legge di Shockley",
+         color = "olivedrab"
+         )
+
+ax.errorbar (V_1, I_1,
+             yerr = sigma_I_conf1,
+             color = "indigo",
+             linestyle = "None",
+             marker = 'o',
+             capsize = 4,
+             label = "Dati osservati"
+             )
+
+plt.legend ()
+plt.grid (True)
+plt.show ()
